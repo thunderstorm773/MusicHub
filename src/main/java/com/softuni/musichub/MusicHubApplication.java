c167bb01d6b93fa71ca16717ed5374432ec2a13f
thunderstorm773 <luko193@gmail.com>
2018-03-08 00:48:59 +0200

    Set Up Spring Project.

---
+++ src/main/java/com/softuni/musichub/MusicHubApplication.java
@@ -0,0 +1,12 @@
+package com.softuni.musichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicHubApplication.class, args);
+	}
+}
