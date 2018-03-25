83400fdb84125cfc337f5831039ec978a52f023d
thunderstorm773 <luko193@gmail.com>
2018-03-25 23:40:26 +0300

    Small style changes in audio player.

--- src/main/java/com/softuni/musichub/admin/controller/AdminController.java
+++ src/main/java/com/softuni/musichub/admin/controller/AdminController.java
@@ -26,12 +26,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/admin")
