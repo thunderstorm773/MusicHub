94e3834105fc7fa45298caa7fff3916698ddd7d5
Luko Delidzhov <luko193@gmail.com>
2024-11-22 21:13:56 +0200

    + Настройка за автоматично отваряне на Google Chrome с въведен адрес на приложението при стартирането на MusicHub, чрез Spring Boot

--- src/main/java/com/softuni/musichub/MusicHubApplication.java
+++ src/main/java/com/softuni/musichub/MusicHubApplication.java
@@ -3,10 +3,15 @@ package com.softuni.musichub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MusicHubApplication {
 
-	public static void main(String[] args) {
+	private static final String STARTING_URL = "http://localhost:8080";
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MusicHubApplication.class, args);
+
+		Runtime.getRuntime().exec("cmd /c start chrome " + STARTING_URL);
 	}
 }
