d3e7523abbf3cd1779f47edc77674e6b0f07c0b0
Luko Delidzhov <luko193@gmail.com>
2024-11-22 20:57:14 +0200

    + Добавяне на config файл за използване на Cloudinary CDN(без специфичните данни за api key)

---
+++ src/main/java/com/softuni/musichub/config/CdnConfigData.java
@@ -0,0 +1,16 @@
+package com.softuni.musichub.config;
+
+public class CdnConfigData {
+
+    public static final String CLOUD_NAME_KEY = "cloud_name";
+
+    public static final String CLOUD_NAME_VALUE = "";
+
+    public static final String API_KEY = "api_key";
+
+    public static final String API_KEY_VALUE = "";
+
+    public static final String API_SECRET_KEY = "api_secret";
+
+    public static final String API_SECRET_VALUE = "";
+}
