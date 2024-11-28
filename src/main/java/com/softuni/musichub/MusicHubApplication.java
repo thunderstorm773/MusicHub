1dfc449d0d9c9c7d7339aacf4e9b34a20e048ea0
Luko Delidzhov <luko193@gmail.com>
2024-11-28 19:48:44 +0200

    + removed platform specific browser run on application start

--- src/main/java/com/softuni/musichub/MusicHubApplication.java
+++ src/main/java/com/softuni/musichub/MusicHubApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicHubApplication {
 
-	private static final String STARTING_URL = "http://localhost:8080";
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MusicHubApplication.class, args);
-
-		Runtime.getRuntime().exec("cmd /c start chrome " + STARTING_URL);
 	}
 }
