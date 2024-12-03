eeb6de611e92c9027c430b75a4791f9085965c0c
Luko Delidzhov <luko193@gmail.com>
2024-12-03 10:42:46 +0200

    + Showing username in header fragment when user is logged in with Google SSO.

--- src/main/java/com/tu/musichub/config/BeanConfig.java
+++ src/main/java/com/tu/musichub/config/BeanConfig.java
@@ -2,6 +2,7 @@ package com.tu.musichub.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.tu.musichub.user.utils.UserUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,5 +18,10 @@ public class BeanConfig {
     @Bean
     public Gson getGson() {
         return new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    @Bean
+    public UserUtils userUtils() {
+        return new UserUtils();
     }
 }
