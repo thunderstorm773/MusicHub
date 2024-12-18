58992eeddc96fc37a41d24b5a6778de3c7df694e
Luko Delidzhov <luko193@gmail.com>
2024-12-18 18:22:49 +0200

    + Removed unnecessary config file + Moved cdn config data to main CdnUtil class

--- src/main/java/com/tu/musichub/util/CdnUtil.java
+++ src/main/java/com/tu/musichub/util/CdnUtil.java
@@ -4,10 +4,11 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.Transformation;
 import com.cloudinary.Url;
 import com.cloudinary.utils.ObjectUtils;
-import com.tu.musichub.config.CdnConfigData;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -29,21 +30,29 @@ public class CdnUtil {
 
     private static final String ATTACHMENT_FLAG = "attachment";
 
-    private final CdnConfigData configData;
+    public static final String CLOUD_NAME_KEY = "cloud_name";
+
+    public static final String API_KEY = "api_key";
+
+    public static final String API_SECRET_KEY = "api_secret";
+
+    @Value("${CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${API_KEY}")
+    private String apiKey;
+
+    @Value("${API_SECRET}")
+    private String apiSecret;
 
     private Cloudinary cloudinary;
 
-    @Autowired
-    public CdnUtil(CdnConfigData configData) {
-        this.configData = configData;
-        this.configCloudinary();
-    }
-
+    @PostConstruct
     private void configCloudinary() {
         Map configMap = ObjectUtils.asMap(
-                CdnConfigData.CLOUD_NAME_KEY, configData.getCloudName(),
-                CdnConfigData.API_KEY, configData.getApiKey(),
-                CdnConfigData.API_SECRET_KEY, configData.getApiSecret()
+                CLOUD_NAME_KEY, this.cloudName,
+                API_KEY, this.apiKey,
+                API_SECRET_KEY, this.apiSecret
         );
 
         this.cloudinary = new Cloudinary(configMap);
@@ -60,16 +69,13 @@ public class CdnUtil {
     }
 
     public String getResourceFullUrl(String partialUrl, String resourceType) {
-        String resourceFullUrl = this.cloudinary.url()
-                .resourceType(resourceType).generate(partialUrl);
-        return resourceFullUrl;
+        return this.cloudinary.url().resourceType(resourceType).generate(partialUrl);
     }
 
     public String getResourceDownloadUrl(String partialUrl, String resourceType) {
         Transformation attachmentFlag = new Transformation().flags(ATTACHMENT_FLAG);
         Url url = this.cloudinary.url().transformation(attachmentFlag);
-        String downloadUrl = url.resourceType(resourceType).generate(partialUrl);
-        return downloadUrl;
+        return url.resourceType(resourceType).generate(partialUrl);
     }
 
     public void deleteResource(String partialUrl) throws Exception {
